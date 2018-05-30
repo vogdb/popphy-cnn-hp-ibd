8a0ce3933038bf4a3eec73546c6cd267f2c94bdf
vogdb <vozhdb@gmail.com>
2018-05-30 14:52:05 +0300

    additional metrics to train_plain

--- src/train.py
+++ src/train.py
@@ -1,14 +1,15 @@
 import argparse
 import json
-
-import keras as keras
-from keras.callbacks import ModelCheckpoint
-import numpy as np
 import os
 
+import keras as keras
+import numpy as np
+import pandas as pd
+from keras.callbacks import ModelCheckpoint
 from keras.layers import Dense, Conv2D, MaxPooling2D, Flatten, Dropout, regularizers
 from keras.optimizers import SGD
 from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.metrics import f1_score, precision_score, recall_score, roc_auc_score, accuracy_score
 from sklearn.model_selection import StratifiedKFold, KFold, cross_val_score
 
 from prepare_data import prepare_dataset
@@ -27,6 +28,7 @@ epochs_num = args.epochs
 batch_size = args.batch_size
 dataset = args.dataset
 
+result_filepath = os.path.join(os.pardir, 'result')
 
 def create_popphy_cnn(input_data):
     rows = input_data.shape[1]  # must be as image Height
@@ -77,41 +79,72 @@ def create_popphy_cnn(input_data):
     return model
 
 
+def plain_train_metrics():
+    class Metrics(keras.callbacks.Callback):
+        def on_train_begin(self, logs={}):
+            self._data = []
+
+        def on_epoch_end(self, batch, logs={}):
+            X_val, y_val = self.validation_data[0], self.validation_data[1]
+            self._data.append(self.calculate(self.model, X_val, y_val))
+            return
+
+        def calculate(self, model, X_val, y_val):
+            y_predict = np.asarray(model.predict(X_val))
+            y_val = np.argmax(y_val, axis=1)
+            y_predict = np.argmax(y_predict, axis=1)
+
+            result = {
+                'val_acc': accuracy_score(y_val, y_predict),
+                'val_recall': recall_score(y_val, y_predict),
+                'val_precision': precision_score(y_val, y_predict),
+                'val_f1': f1_score(y_val, y_predict),
+                'val_roc_auc': roc_auc_score(y_val, y_predict),
+            }
+            return result
+
+        def get_data(self):
+            return self._data
+
+    return Metrics()
+
+
 def train_plain(X, y):
     kfold = StratifiedKFold(n_splits=splits_num, shuffle=True)
     kfold_split = kfold.split(X, y)
     # format to Keras input shape
     X = X.reshape(X.shape + (1,))
     y = keras.utils.to_categorical(y, num_classes=2)
-    val_acc_list = []
-    val_loss_list = []
+    best_metrics_list = []
 
     for index, (train_indices, val_indices) in enumerate(kfold_split):
         X_train, X_val = X[train_indices], X[val_indices]
         y_train, y_val = y[train_indices], y[val_indices]
 
         model = create_popphy_cnn(X)
-        best_model_filepath = os.path.join(os.pardir, 'result', 'cirrhosis' + str(index) + '.hdf5')
+        best_model_filepath = os.path.join(result_filepath, 'cirrhosis' + str(index) + '.hdf5')
+        metrics = plain_train_metrics()
         callback_list = [
-            ModelCheckpoint(best_model_filepath, save_best_only=True, monitor='val_acc', mode='max')
+            ModelCheckpoint(best_model_filepath, save_best_only=True, monitor='val_acc', mode='max'),
+            metrics
         ]
         history = model.fit(X_train, y_train, epochs=epochs_num, batch_size=batch_size, verbose=0,
                             validation_data=(X_val, y_val), callbacks=callback_list)
 
         model.load_weights(best_model_filepath)
-        score = model.evaluate(X_val, y_val)
-        val_loss_list.append(score[0])
-        val_acc_list.append(score[1])
+        best_metrics_list.append(metrics.calculate(model, X_val, y_val))
 
-        history_filepath = os.path.join(os.pardir, 'result', 'cirrhosis_hist' + str(index) + '.txt')
+        history_filepath = os.path.join(result_filepath, 'cirrhosis_hist' + str(index) + '.csv')
         with open(history_filepath, 'w') as hist_file:
-            json.dump(history.history, hist_file)
+            metrics_df = pd.DataFrame(metrics.get_data())
+            metrics_df.drop(['val_acc'], axis=1, inplace=True) # it's duplicated in history_df
+            history_df = pd.DataFrame(history.history)
+            train_df = pd.concat([history_df, metrics_df], axis=1)
+            train_df.to_csv(hist_file, header=True, sep=';', index=False)
 
-    val_acc_list = np.array(val_acc_list)
-    val_loss_list = np.array(val_loss_list)
-    print '\n######################################################################'
-    print 'val_acc_list mean and std: ', val_acc_list.mean(), val_acc_list.std()
-    print 'val_loss_list mean and std: ', val_loss_list.mean(), val_loss_list.std()
+    best_metrics_df = pd.DataFrame(best_metrics_list)
+    best_metrics_filepath = os.path.join(result_filepath, 'best_metrics.csv')
+    best_metrics_df.to_csv(best_metrics_filepath, header=True, sep=';', index=False)
 
 
 def train_cross_val_score(X, y):
@@ -147,3 +180,4 @@ train_plain(X, y)
 
 
 # TODO use a `binary` classifier as an alternative.
+# TODO simple dataset and show that both trains train_cross_val_score and train_plain work equally
