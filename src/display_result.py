e1becb004929bdaa31dbf23b59550aebc34b969a
vogdb <vozhdb@gmail.com>
2018-06-01 20:02:20 +0300

    display result of csv

--- src/display_result.py
+++ src/display_result.py
@@ -1,25 +1,25 @@
-import json
 import os
 import matplotlib.pyplot as plt
+import pandas as pd
 
-stat_name_list = ['acc', 'val_acc']
+# stat_name_list = ['acc', 'val_acc']
+stat_name_list = ['val_recall', 'val_precision']
 index_list = range(10)
-result_dirpath = os.path.join(os.pardir, 'result', '1000')
+result_dirpath = os.path.join(os.pardir, 'result', '400e_usual')
 
 # collect data
-result_list = []
+result_df_list = []
 for i in index_list:
-    i_result_filepath = os.path.join(result_dirpath, 'cirrhosis_hist' + str(i) + '.txt')
-    with open(i_result_filepath) as result_file:
-        result_dict = json.load(result_file)
-        result_list.append(result_dict)
+    i_result_filepath = os.path.join(result_dirpath, 'cirrhosis_hist' + str(i) + '.csv')
+    i_df = pd.read_csv(i_result_filepath, sep=';')
+    result_df_list.append(i_df)
 
 # display data
 plt.figure(figsize=(14, 8))
-for i, result_dict in enumerate(result_list):
+for i, i_df in enumerate(result_df_list):
     plt.subplot(2, 5, i + 1)
-    plt.plot(result_dict[stat_name_list[0]], 'b-', label=stat_name_list[0])
-    plt.plot(result_dict[stat_name_list[1]], 'c-', label=stat_name_list[1])
+    plt.plot(i_df[stat_name_list[0]], 'b-', label=stat_name_list[0])
+    plt.plot(i_df[stat_name_list[1]], 'c-', label=stat_name_list[1])
     plt.legend()
 
 plt.tight_layout()
